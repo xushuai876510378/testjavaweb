85d23d4b7a4851348602839708be065d4396e0a8
徐帅 <xushuai@yondervision.com.cn>
2019-08-16 16:35:14 +0800

    对方是否

---
+++ src/Servlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * AUTHOR:XUSHUAI
+ * TIME:2019/8/1615:34
+ * DESCRIBE:servlet
+ */
+
+public class Servlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("进入servletdopost方法！");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+}
